8c106c4a544ce8841b43efbf800e75f1b25ee190
Rafael Irgolic <hello@irgolic.com>
2023-04-10 22:48:24 +0200

    main: Consolidate parameters into pydantic BaseSettings

--- autopr/gh_actions_entrypoint.py
+++ autopr/gh_actions_entrypoint.py
@@ -1,11 +1,10 @@
-import json
 import os
+from typing import Any
+from autopr.main import main, Settings
 
-import requests
+import yaml
 
 from autopr.log_config import configure_logging
-from autopr.models.artifacts import Issue, Message
-from autopr.services.event_service import EventService
 
 configure_logging()
 
@@ -13,38 +12,23 @@ import structlog
 log = structlog.get_logger()
 
 
+class GithubActionSettings(Settings):
+    class Config:
+        env_prefix = 'INPUT_'
+
+        @classmethod
+        def parse_env_var(cls, field_name: str, raw_val: str) -> Any:
+            if field_name in ['pull_request_agent', 'codegen_agent']:
+                return yaml.safe_load(raw_val)
+            return cls.json_loads(raw_val)  # type: ignore
+
+
 if __name__ == '__main__':
     log.info("Starting gh_actions_entrypoint.py")
 
     repo_path = os.environ['GITHUB_WORKSPACE']
-    os.environ['OPENAI_API_KEY'] = os.environ['INPUT_OPENAI_API_KEY']
-
-    inputs = {
-        'event_name': os.environ['INPUT_EVENT_NAME'],
-        'event': json.loads(os.environ['INPUT_EVENT']),
-        'github_token': os.environ['INPUT_GITHUB_TOKEN'],
-        'base_branch': os.environ['INPUT_BASE_BRANCH'],
-        'model': os.environ['INPUT_MODEL'],
-        'context_limit': int(os.environ['INPUT_CONTEXT_LIMIT']),
-        'min_tokens': int(os.environ['INPUT_MIN_TOKENS']),
-        'max_tokens': int(os.environ['INPUT_MAX_TOKENS']),
-        'num_reasks': int(os.environ['INPUT_NUM_REASKS']),
-        'pull_request_agent_id': os.environ['INPUT_PULL_REQUEST_AGENT_ID'],
-        'codegen_agent_id': os.environ['INPUT_CODEGEN_AGENT_ID'],
-    }
-    inputs.update({
-        k: v
-        for k, v in os.environ.items()
-        if k.startswith('INPUT_')
-        and k[6:].lower() not in inputs
-        and not any(s in k for s in [
-            'API_KEY',
-            'EVENT',
-        ])
-    })
-
-    from autopr.main import main
+    settings = GithubActionSettings.parse_obj({})  # pyright workaround
     main(
         repo_path=repo_path,
-        **inputs
+        settings=settings,
     )
