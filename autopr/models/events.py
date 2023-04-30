ba50ec59a7959f01c3e1ebd283ec4035b8ad7a9e
Rafael Irgolic <hello@irgolic.com>
2023-04-30 19:27:23 +0200

    models: Add docstrings

--- autopr/models/events.py
+++ autopr/models/events.py
@@ -6,16 +6,22 @@ from autopr.models.artifacts import Issue, Message
 
 
 class Event(pydantic.BaseModel):
+    """
+    Events trigger AutoPR to run in different ways.
+
+    TODO implement more event types (i.e., code review)
+    """
     event_type: str
 
 
 class IssueLabeledEvent(Event):
+    """
+    Event triggered when a label is added to an issue.
+    """
     event_type: Literal['issue_opened'] = 'issue_opened'
 
     issue: Issue
     label: str
 
-
-# TODO implement more event types (i.e., code review)
 
 EventUnion = IssueLabeledEvent
