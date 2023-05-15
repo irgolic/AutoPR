7edcd4e38f2e566dae2fab87d035c7f18dd5206e
Rafael Irgolic <hello@irgolic.com>
2023-05-15 22:44:18 +0200

    request_more_info: Init action

---
+++ autopr/actions/request_more_info.py
@@ -0,0 +1,28 @@
+from autopr.actions.base import Action, ContextDict
+from autopr.models.artifacts import Issue
+
+
+class RequestMoreInfo(Action):
+    id = "request_more_information"
+    description = "Request more information from the user."
+
+    class Arguments(Action.Arguments):
+        message: str
+
+        output_spec = "<string name='message'/>"
+
+    def run(self, arguments: Arguments, context: ContextDict) -> ContextDict:
+        # Get the issue from the context
+        issue = context['issue']
+        if not isinstance(issue, Issue):
+            self.log.error(f"Expected issue to be of type Issue, got {type(issue)}")
+            raise TypeError(f"Expected issue to be of type Issue, got {type(issue)}")
+
+        # Get the message from the arguments
+        message = arguments.message
+
+        # Add a comment to the issue
+        self.publish_service.comment_on_issue(message)
+
+        # Return the context
+        return context
