8be3046c843236a0af3304f6e6a18e9e0b32af7a
tjazerzen <erzen.tjaz@gmail.com>
2023-09-08 12:07:13 +0200

    match statement --> if statement in publish_issue

--- autopr/actions/publish_issue.py
+++ autopr/actions/publish_issue.py
@@ -17,7 +17,7 @@ class Inputs(BaseModel):
     issue_body: str
 
     # Whether to duplicate the issue if its title already exists
-    skip_if_exists: bool = True
+    update_if_exists: bool = True
 
 
 class Outputs(BaseModel):
@@ -33,14 +33,13 @@ class PublishIssue(Action[Inputs, Outputs]):
 
     async def run(self, inputs: Inputs) -> Outputs:
         issue = await self.platform_service.get_issue_by_title(inputs.issue_title)
-        match issue:
-            case None:
-                issue_number = await self.platform_service.create_issue(inputs.issue_title, inputs.issue_body)
-                return Outputs(issue_number=issue_number)
-            case _:
-                await self.platform_service.update_issue_body(issue.number, inputs.issue_body)
-                return Outputs(issue_number=issue.number)
-
+        if issue is None:
+            issue_number = await self.platform_service.create_issue(inputs.issue_title, inputs.issue_body)
+            return Outputs(issue_number=issue_number)
+        elif issue is not None and inputs.update_if_exists:
+            await self.platform_service.update_issue_body(issue.number, inputs.issue_body)
+            return Outputs(issue_number=issue.number)
+        return Outputs(issue_number=None)
 
 if __name__ == "__main__":
     # IMPORTANT: Here, method publish_issue is mocked and does not actually publish an issue to a repository.
@@ -53,7 +52,7 @@ if __name__ == "__main__":
                 inputs=Inputs(
                     issue_title="Test Issue",
                     issue_body="This is a test issue",
-                    skip_if_exists=True,
+                    update_if_exists=True,
                 ),
             )
         )
