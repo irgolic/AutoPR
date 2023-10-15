0e145af13755d2172b5fd65e41ac6f0d60b511de
Rafael Irgolic <hello@irgolic.com>
2023-10-15 23:00:46 +0100

    find_todos: comment => language

--- autopr/actions/find_todos.py
+++ autopr/actions/find_todos.py
@@ -1,20 +1,22 @@
 import asyncio
 import os
 import re
+from collections import defaultdict
+from typing import Literal, Optional
 from unittest.mock import patch
 import pydantic
 from autopr.actions.base import Action
 
 from autopr.services.platform_service import PlatformService
 from tree_sitter_languages import get_parser
+from tree_sitter import Parser
 
 
 TODO_ISSUE_BODY = re.compile(r'<!--\ninfo: AutoPR fingerprint\nissue type: TODO\ntask: [^\n]*\n-->', re.DOTALL)
 
 comment_treesitter_language_mapping = {
-    "#": "python",
-    "//": "javascript",
-    "/*": "javascript",
+    "python": ["#"],
+    "javascript": ["//", "/*"],
 }
 
 
@@ -28,10 +30,11 @@ class TodoLocation(pydantic.BaseModel):
 class Todo(pydantic.BaseModel):
     task: str
     locations: list[TodoLocation]
+    issue_number: Optional[int] = None
 
 
 class Inputs(pydantic.BaseModel):
-    comment: str = "#"
+    language: Literal[tuple(comment_treesitter_language_mapping.keys())] = "python"  # type: ignore
     todo_keywords: list[str] = ["TODO", "FIXME"]
 
 
@@ -48,14 +51,14 @@ class FindTodos(Action[Inputs, Outputs]):
     id = "find_todos"
 
     @staticmethod
-    def is_binary(path):
+    def is_binary(path: str):
         return b"\x00" in open(path, "rb").read(1024)
 
-    async def process_file(self, file, inputs, parser) -> dict[str, list[TodoLocation]]:
+    async def process_file(self, file: str, todo_keywords: list[str], comment_keywords: list[str], parser: Parser) -> dict[str, list[TodoLocation]]:
         if self.is_binary(file):
             return {}
 
-        task_to_locations = {}
+        task_to_locations = defaultdict(list)
 
         with open(file, "r", encoding="utf-8", errors="ignore") as f:
             file_content = f.read()
@@ -64,30 +67,37 @@ class FindTodos(Action[Inputs, Outputs]):
         tree = parser.parse(bytes(file_content, "utf8"))
         root_node = tree.root_node
 
-        # A simple recursive function to traverse all nodes and find comments
+        async def process_comment(node):
+            comment_text = file_content[node.start_byte:node.end_byte].strip()
+            combined_todo_keywords = "|".join(todo_keywords)
+
+            for comment in comment_keywords:
+                pattern = re.compile(rf'^{re.escape(comment)}\s*({combined_todo_keywords})')
+
+                if not pattern.search(comment_text):
+                    continue
+
+                # Check if this comment continues in the next line
+                task = re.sub(rf'^{re.escape(comment)}\s*', '', comment_text)
+                start_line = node.start_point[0] + 1
+                end_line = node.end_point[0] + 1
+                next_node = node.next_named_sibling
+                if next_node:
+                    comment_text_next = file_content[next_node.start_byte:next_node.end_byte].strip()
+                    comment_pattern_next = re.compile(rf'^{re.escape(comment)}\s{{2,}}(.+)$')
+                    while next_node and next_node.type == "comment" and node.start_point[0] + 1 == \
+                            next_node.start_point[0] and comment_pattern_next.search(comment_text_next):
+                        task += " " + re.sub(rf'^{re.escape(comment)}\s*', '', comment_text_next)
+                        end_line = next_node.end_point[0] + 1
+                        next_node = next_node.next_named_sibling
+
+                location = await self.get_todo_location(file, start_line, end_line)
+                task_to_locations[task].append(location)
+
         async def traverse(node):
-            nonlocal file_content
+            """A simple recursive function to traverse all nodes and find comments"""
             if node.type == "comment":
-                comment_text = file_content[node.start_byte:node.end_byte].strip()
-                combined_todo_keywords = "|".join(inputs.todo_keywords)
-                pattern = re.compile(rf'^{re.escape(inputs.comment)}\s*({combined_todo_keywords})')
-
-                if pattern.search(comment_text):
-                    # Check if this comment continues in the next line
-                    task = re.sub(rf'^{re.escape(inputs.comment)}\s*', '', comment_text)
-                    start_line = node.start_point[0] + 1
-                    end_line = node.end_point[0] + 1
-                    next_node = node.next_named_sibling
-                    if next_node:
-                        comment_text_next = file_content[next_node.start_byte:next_node.end_byte].strip()
-                        comment_pattern_next = re.compile(rf'^{re.escape(inputs.comment)}\s{{2,}}(.+)$')
-                        while next_node and next_node.type == "comment" and node.start_point[0] + 1 == next_node.start_point[0] and comment_pattern_next.search(comment_text_next):
-                            task += " " + re.sub(rf'^{re.escape(inputs.comment)}\s*', '', comment_text_next)
-                            end_line = next_node.end_point[0] + 1
-                            next_node = next_node.next_named_sibling
-
-                    location = await self.get_todo_location(file, start_line, end_line)
-                    task_to_locations.setdefault(task, []).append(location)
+                await process_comment(node)
 
             # Recursively traverse children
             for child in node.children:
@@ -107,8 +117,9 @@ class FindTodos(Action[Inputs, Outputs]):
         closed_issue_bodies = [closed_issue.messages[0].body for closed_issue in closed_issues_list]
         filtered_todos = [
             todo for todo in todos 
-            if not any(todo.task in closed_issue_body for closed_issue_body in closed_issue_bodies)
-            ]
+            if not any(todo.task in closed_issue_body
+                       for closed_issue_body in closed_issue_bodies)
+        ]
         return filtered_todos
     
     @staticmethod
@@ -127,10 +138,11 @@ class FindTodos(Action[Inputs, Outputs]):
 
     async def run(self, inputs: Inputs) -> Outputs:
         # Set the parsing language
-        if not comment_treesitter_language_mapping.get(inputs.comment):
-            raise ValueError(f"Comment {inputs.comment} not supported. Current supported comments are: {', '.join(list(comment_treesitter_language_mapping.keys()))}")
-        
-        parser = get_parser(comment_treesitter_language_mapping[inputs.comment])
+        if inputs.language not in comment_treesitter_language_mapping:
+            raise ValueError(f"Language {inputs.language} not supported")
+
+        comment_keywords = comment_treesitter_language_mapping[inputs.language]
+        parser = get_parser(inputs.language)
         current_dir = os.getcwd()
         all_task_to_locations = {}
 
@@ -142,7 +154,7 @@ class FindTodos(Action[Inputs, Outputs]):
 
             for file in files:
                 relative_path = os.path.relpath(os.path.join(root, file), current_dir)
-                file_task_to_locations = await self.process_file(relative_path, inputs, parser)
+                file_task_to_locations = await self.process_file(relative_path, inputs.todo_keywords, comment_keywords, parser)
 
                 for task, locations in file_task_to_locations.items():
                     all_task_to_locations.setdefault(task, []).extend(locations)
