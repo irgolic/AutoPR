099ec8454684f7a17d8de595efcd0833176a55dc
Mike P. Sinn <2808553+mikepsinn@users.noreply.github.com>
2023-12-18 13:17:55 -0600

    Fixed editUrl in Docusauraus so Edit button works

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -44,7 +44,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/irgolic/AutoPR/tree/main/website/',
         },
         blog: false,
         // blog: {
